a6fa127c85f8b4a8083f5ea10b2339cc29283086
git ola <gitgmail.com>
2025-10-23 19:41:23 -0500

    agregacion de timestampmodel

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+###asdasdasdasdsa
+
+
+
+###asdasdasdasdsa###asdasdasdasdsa
+###asdasdasdasdsa
+###asdasdasdasdsa
+###asdasdasdasdsa
+###asdasdasdasdsa
+###asdasdasdasdsa
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class TimestampModel(models.Model):
+    """
+    Un modelo base abstracto que proporciona campos de auditoría
+    (fecha de creación y actualización) para otros modelos.
+    """
+    fecha_creacion = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Fecha de creación"
+    )
+    fecha_actualizacion = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Fecha de actualización"
+    )
+
+    class Meta:
+        # Esto es clave: le dice a Django que este modelo es "abstracto".
+        # No se creará una tabla en la base de datos para TimestampModel.
+        # Solo se usará para añadir sus campos a otros modelos.
+        abstract = True
